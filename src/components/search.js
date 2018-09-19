7d7290c9875f3c6daea578c16367b36a103c6568
Lucas Mazzutti <lucas.mazzutti@booking.com>
2018-09-19 14:46:14 +0100

    adding format

--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export default () => (
-  <div className='tc pv4 pv5-ns center'>
+  <div className='tc pv4 pv5-ns w-50 center'>
     <form className="form-inline" action="/weather" method="POST">
             <label className='fw6 underline' for="city">City</label>
-            <input type="text" class="form-control" name="city" placeholder="e.g.: London"/>
+            <input type="text" class="b pa3 input-reset ba bg-transparent hover-bg-black hover-white w-75" name="city" placeholder="e.g.: London"/>
             <input className="" type="submit" value="Check Weather!"/>
         </form>
   </div>
